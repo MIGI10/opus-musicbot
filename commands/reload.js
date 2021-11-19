42f29b67786ecb7d6837920f3968bdb49ef9ecc4
MIGI28 <miguelpalau1@gmail.com>
2021-11-19 23:25:25 +0100

    added spotify module and lang files to reload cmd

--- commands/reload.js
+++ commands/reload.js
@@ -2,23 +2,34 @@ const loadCommands = require('../structures/command');
 const loadEvents = require('../structures/event');
 const { readdirSync } = require('fs');
 const { join } = require('path');
-const filePath = join(__dirname, "..", "events");
+const eventsFolder = join(__dirname, "..", "events");
 
 module.exports.run = async (client, message, args) => {
+
+    delete require.cache['/usr/src/bot/spotify/req-content.js'];
 
     await client.commands.clear();
     await client.cmdaliases.clear();
 
     loadCommands.run(client);
 
-    for await (const eventFile of readdirSync(filePath)) {
+    for await (const eventFile of readdirSync(eventsFolder)) {
         const eventName = eventFile.split(".").shift();
         client.removeAllListeners(eventName);
     }
 
     loadEvents.run(client);
 
-    message.channel.send('Successfully reloaded events and commands!');
+    delete require.cache['/usr/src/bot/lang/eng.json'];
+    delete require.cache['/usr/src/bot/lang/spa.json'];
+
+    const engStrings = require('/usr/src/bot/lang/eng.json');
+    const spaStrings = require('/usr/src/bot/lang/spa.json');
+
+    strings['eng'] = engStrings;
+    strings['spa'] = spaStrings;
+
+    message.channel.send('Successfully reloaded commands, events, lang and spotify module!');
 }
 
 module.exports.info = {
